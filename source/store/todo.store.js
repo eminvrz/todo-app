ae0a12e193307be6c9e22edc6d4de75135378805
Emiliano <emiliano.nvrz@gmail.com>
2023-01-29 11:34:29 -0800

    Implementacion de la funcionalidad de cada metodo

--- source/store/todo.store.js
+++ source/store/todo.store.js
@@ -12,6 +12,8 @@ const state = {
         new Todo('Piedra del alma'),
         new Todo('Piedra del infinito'),
         new Todo('Piedra del tiempo'),
+        new Todo('Piedra del poder'),
+        new Todo('Piedra del realidad'),
     ],
     filter: Filters.All,
 }
@@ -25,12 +27,29 @@ const loadStore = () => {
     throw new Error('Not implemented');
 }
 
+const getTodos = (filter = Filters.All) => {
+    switch ( filter ){
+        case Filters.All:
+            return [...state.todos]; // regresar un nuevo arreglo con cada uno de sus valores
+
+        case Filters.Completed:
+            return state.todos.filter(todo => todo.done); // Los que esten hechos
+
+        case Filters.Pending:
+            return state.todos.filter(todo => !todo.done); // Los que esten en negacion 
+
+        default:
+            throw new Error(`Option ${ filter } is not valid.`);
+    }
+}
+
 /**
  * 
  * @param {String} description 
  */
 const addTodo = ( description ) => {
-    throw new Error('Not implemented');
+    if( !description) throw new Error('Description is required');
+    state.todos.push( new Todo(description));
 }
 
 /**
@@ -38,7 +57,13 @@ const addTodo = ( description ) => {
  * @param {String} todoId 
  */
 const toogleTodo = ( todoId ) => {
-    throw new Error('Not implemented');
+    // con este map barremos todos los esten en el arreglo hasta encontrar el todoId
+    state.todos = state.todos.map( todo => {
+        if( todo.id === todoId) {
+            todo.done = !todo.done; // cambiar el valor opuesto que tenga el valor todo.done
+        }
+        return todo;
+    });
 }
 
 /**
@@ -46,26 +71,32 @@ const toogleTodo = ( todoId ) => {
  * @param {String} todoId 
  */
 const deleteTodo = ( todoId ) => {
-    throw new Error('Not implemented');
+    state.todos = state.todos.filter( todo => todo.id !== todoId); // Eliminar el todoid que nos esta mandando, diferente a todos los demas
 }
 
 const deleteCompleted = () => {
-    throw new Error('Not implemented');
+    state.todos = state.todos.filter( todo => todo.done); // Eliminar todos los que esten completados
 }
 
+/**
+ * 
+ * @param {Filters} newFilter 
+ */
 const setFilter = (newFilter = Filters.All) => {
-    throw new Error('Not implemented');
+    state.filter = newFilter;
 }
 
 const getCurrentFilter = () => {
-    throw new Error('Not implemented');
+    return state.filter;
 }
 
 
 export default {
+    addTodo,
     deleteCompleted,
     deleteTodo,
     getCurrentFilter,
+    getTodos,
     initStore,
     loadStore,
     setFilter,
