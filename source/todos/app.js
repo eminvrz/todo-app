db09c61b8ecb7a3b78dfa03ee0b13207b3aae0f1
Emiliano <emiliano.nvrz@gmail.com>
2023-01-29 18:29:48 -0800

    Toggle todos

--- source/todos/app.js
+++ source/todos/app.js
@@ -6,6 +6,7 @@ import { renderTodos } from './use-cases'
 const ElementIDs = {
     TodoList: '.todo-list',
     NewTodoInput: '#new-todo-input',
+
 }
 /**
  * 
@@ -29,8 +30,10 @@ export const App = (elementId) => {
 
     // Referencias HTML
     const newDescriptionInput = document.querySelector( ElementIDs.NewTodoInput );
+    const todoListUL = document.querySelector(ElementIDs.TodoList);
 
     // Listeners
+        // para agregar un nuevo todo
     newDescriptionInput.addEventListener('keyup', ( event ) => {
         if( event.keyCode !== 13 ) return;
         if( event.target.value.trim().length === 0) return;
@@ -38,6 +41,13 @@ export const App = (elementId) => {
         todoStore.addTodo( event.target.value );
         displayTodos();
         event.target.value = '';
+    });
+
+        // para marcar si ya esta echo ese todo o viceversa
+    todoListUL.addEventListener('click', (event) => {
+        const element = event.target.closest('[data-id]');
+        todoStore.toogleTodo(element.getAttribute('data-id'));
+        displayTodos();
     })
 
 }
