d62f7393fcbda4748752e2266535c82c577ad6d2
Emiliano <emiliano.nvrz@gmail.com>
2023-01-30 11:57:57 -0800

    Mostrar cantidad de TODOs Pendientes

--- source/todos/app.js
+++ source/todos/app.js
@@ -1,6 +1,6 @@
 import todoStore, { Filters } from "../store/todo.store"
 import html from './app.html?raw'
-import { renderTodos } from './use-cases'
+import { renderTodos, renderPending } from './use-cases'
 
 
 const ElementIDs = {
@@ -8,6 +8,7 @@ const ElementIDs = {
     TodoList: '.todo-list',
     NewTodoInput: '#new-todo-input',
     TodoFilters: '.filtro',
+    PendingCountLabel: '#pending-count',
 }
 /**
  * 
@@ -19,6 +20,11 @@ export const App = (elementId) => {
         const todos = todoStore.getTodos( todoStore.getCurrentFilter());
         //console.log(todos);
         renderTodos( ElementIDs.TodoList, todos);
+        updatePendingCount();
+    }
+
+    const updatePendingCount = () => {
+        renderPending(ElementIDs.PendingCountLabel);
     }
 
     // Cuando la funcion  App() se llama
